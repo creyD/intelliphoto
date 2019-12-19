0bcbe13f1316f715b2192af071cd708c0eb13e71
Conrad <grosserconrad@gmail.com>
2019-12-19 18:38:38 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include <QApplication>
 #include <QDebug>
 #include "IntelliHelper/IntelliHelper.h"
-#include<vector>
+#include <vector>
 
 int main(int argc, char *argv[]){
     // The main application
     QApplication app(argc, argv);
 
-    //some nice ass looking comment
     // Create and open the main window
     IntelliPhotoGui window;
     window.show();
