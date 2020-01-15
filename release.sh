5ef800fdd5ca02ade455a931aabb2cb5c7097173
Conrad <grosserconrad@gmail.com>
2020-01-15 14:16:59 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,7 +7,9 @@ echo "Running Uncrustify..."
 find . \( -name "*.cpp" -o -name "*.c" -o -name "*.h" \) -exec uncrustify -c conf/uncrustify.cfg --no-backup {}
 echo "Uncrustify finished."
 # Run Unit Tests
+echo "Running unit tests..."
 
+echo "Finished unit testing."
 # Remove Overhead Folders/ Files
 echo "Cleaning directory..."
 rm -r src/build-*
@@ -19,3 +21,6 @@ echo "Directory cleaned."
 echo "Running Doxygen..."
 doxygen conf/intelliphoto_dox
 echo "Doxygen finished."
+# Building
+qmake src/IntelliPhoto.pro -spec macx-clang CONFIG+=x86_64 CONFIG+=qtquickcompiler && make qmake_all
+make clean -j4 build-IntelliPhoto-Desktop_Qt_5_14_0_clang_64bit-Release
